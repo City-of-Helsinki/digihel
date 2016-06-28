2693da2efe865c20b9f52b46d5faf54d82b4b0e5
Juha Yrjölä <juha.yrjola@iki.fi>
2016-06-28 12:53:19 +0300

    Update settings.py

--- digihel/settings.py
+++ digihel/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'users',
     'helusers',
     'content',
+    'kehmet',
     'search',
 
     'wagtail.wagtailforms',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'wagtail.wagtailsearch',
     'wagtail.wagtailadmin',
     'wagtail.wagtailcore',
+    'wagtail.contrib.modeladmin',
 
     'modelcluster',
     'taggit',
