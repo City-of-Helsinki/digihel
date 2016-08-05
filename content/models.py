40bd08533e3220e0681a14d4dcf01fc3da4e6256
Juha Yrjölä <juha.yrjola@iki.fi>
2016-08-05 12:04:26 +0300

    Implement a two-column stream field block

--- content/models.py
+++ content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import StreamField
@@ -8,11 +9,28 @@ from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailsearch import index
 
 
+content_blocks = [
+    ('heading', blocks.CharBlock(classname="full title")),
+    ('paragraph', blocks.RichTextBlock()),
+    ('image', ImageChooserBlock()),
+]
+
+
+class TwoColumnBlock(blocks.StructBlock):
+    left_column = blocks.StreamBlock(content_blocks, icon='arrow-left',
+                                     label=_('Left column content'))
+    right_column = blocks.StreamBlock(content_blocks, icon='arrow-right',
+                                      label=_('Right column content'))
+
+    class Meta:
+        icon = 'placeholder'
+        label = _('Two columns')
+        template = 'content/blocks/two_column.html'
+
+
 class ContentPage(Page):
-    body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
+    body = StreamField(content_blocks + [
+        ('two_columns', TwoColumnBlock()),
     ])
 
     content_panels = Page.content_panels + [
