3e99f8a8c97184449f83cff2184cc4eeb54528da
Juha Yrjölä <juha.yrjola@iki.fi>
2016-08-10 18:14:58 +0300

    Make images under the "fullwidth" format full-size
    Fixes #6.

---
+++ digi/image_formats.py
@@ -0,0 +1,8 @@
+from wagtail.wagtailimages.formats import FORMATS
+
+
+# Monkey patch the default formats to always use the
+# original image instead of a width-constrained version.
+for f in FORMATS:
+    if f.name == 'fullwidth':
+        f.filter_spec = 'original'
