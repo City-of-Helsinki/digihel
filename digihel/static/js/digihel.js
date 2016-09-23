c0cdbd466204ea9d7dea3ca04422ca152f4a2449
Tero Tikkanen <tero.tikkanen@gmail.com>
2016-09-23 15:56:24 +0300

    Improve search template and search box

--- digihel/static/js/digihel.js
+++ digihel/static/js/digihel.js
@@ -1,23 +1,27 @@
-$(function() { 
+$(function() {
 
     $('#js-toggle-search, .navbar .drop-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
-		event.preventDefault();
-		$('.navbar .drop-search .input-group > input').val('');
-		$('.navbar .drop-search').toggleClass('is-open');
-		$('a[href="#toggle-search"]').closest('li').toggleClass('active');
+			event.preventDefault();
+			$('.navbar .drop-search .input-group > input').val('');
+			$('.navbar .drop-search').toggleClass('is-open');
+			$('a[href="#toggle-search"]').closest('li').toggleClass('active');
+			$('#js-toggle-search span').removeClass('glyphicon-remove');
+			$('#js-toggle-search span').addClass('glyphicon-search');
 
-		if ($('.navbar .drop-search').hasClass('is-open')) {
-			setTimeout(function() { 
-				$('.navbar .drop-search .form-control').focus();
-			}, 100);
-		}			
-	});
+			if ($('.navbar .drop-search').hasClass('is-open')) {
+				setTimeout(function() {
+					$('.navbar .drop-search .form-control').focus();
+				}, 100);
+				$('#js-toggle-search span').removeClass('glyphicon-search');
+				$('#js-toggle-search span').addClass('glyphicon-remove');
+			}
+		});
 
-	$(document).on('keyup', function(event) {
-		if (event.which == 27 && $('.navbar .drop-search').hasClass('is-open')) {
-			$('#js-toggle-search').trigger('click');
-		}
-	});
+		$(document).on('keyup', function(event) {
+			if (event.which == 27 && $('.navbar .drop-search').hasClass('is-open')) {
+				$('#js-toggle-search').trigger('click');
+			}
+		});
 
     $('.match-height').matchHeight();
 
