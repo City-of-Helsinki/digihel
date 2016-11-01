36fee1a21115d69a3ce76602ef6b54867ac2d422
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-11-01 14:00:07 +0200

    Add wagtailtinymce (a custom fork that allows better customization)
    * Adds the anchor plugin (fixes #38)
    * Span and div styles can be set, though are not configured (enables #42)
    * Allows linking within tables (fixes #37)

---
+++ digihel/tinymce.py
@@ -0,0 +1,55 @@
+import json
+
+from wagtailtinymce.rich_text import TinyMCERichTextArea
+
+
+class DigiHelTinyMCERichTextArea(TinyMCERichTextArea):
+    plugins = [
+        'advlist',
+        'anchor',
+        'autolink',
+        'charmap',
+        'code',
+        'codesample',
+        'contextmenu',
+        'directionality',
+        'emoticons',
+        'fullscreen',
+        'hr',
+        'image',
+        'imagetools',
+        'insertdatetime',
+        'link',
+        'lists',
+        'media',
+        'nonbreaking',
+        'pagebreak',
+        'paste',
+        'preview',
+        'print',
+        'searchreplace',
+        'table',
+        'template',
+        'textpattern',
+        'visualblocks',
+        'visualchars',
+        'wordcount',
+    ]
+    default_options = {
+        'image_advtab': True,
+        'browser_spellcheck': True,
+        'noneditable_leave_contenteditable': True,
+        'language': 'en',
+        'language_load': True,
+        'content_css': [
+            # add static URLs here?
+        ]
+    }
+
+    def build_js_init_arguments(self):
+        args = super(DigiHelTinyMCERichTextArea, self).build_js_init_arguments()
+        args.update(
+            plugins=self.plugins,
+        )
+        args.pop('menubar', None)  # Always enable the menubar
+        return args
