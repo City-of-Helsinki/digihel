dabbe2ce3ed7b720284ca1fc41d675b030e94bc2
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-11-01 10:27:42 +0200

    Use PostgreSQL by default

--- digihel/settings.py
+++ digihel/settings.py
@@ -117,8 +117,9 @@ WSGI_APPLICATION = 'digihel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'digihel',
+        'USER': os.environ.get('DATABASE_USER', 'digihel'),
     }
 }
 
