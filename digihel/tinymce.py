91d7ace46a8c0c6a2f7a7fd0686d7e0644fafff7
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-11-03 16:29:31 +0200

    Add TinyMCE content stylesheet

--- digihel/tinymce.py
+++ digihel/tinymce.py
@@ -1,5 +1,7 @@
 import json
 
+from django.templatetags.static import static
+
 from wagtailtinymce.rich_text import TinyMCERichTextArea
 
 
@@ -41,9 +43,7 @@ class DigiHelTinyMCERichTextArea(TinyMCERichTextArea):
         'noneditable_leave_contenteditable': True,
         'language': 'en',
         'language_load': True,
-        'content_css': [
-            # add static URLs here?
-        ]
+        'content_css': []
     }
 
     def build_js_init_arguments(self):
@@ -52,4 +52,5 @@ class DigiHelTinyMCERichTextArea(TinyMCERichTextArea):
             plugins=self.plugins,
         )
         args.pop('menubar', None)  # Always enable the menubar
+        args['content_css'].append(static('css/tinymce-content.css'))
         return args
