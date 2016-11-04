0b267ed312866d32b56d1cad76da8d3e71745c5b
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-11-04 14:53:07 +0200

    Add Wagtail hooks to allow various further elements through its whitelister
    Fixes #61

---
+++ digihel/wagtail_hooks.py
@@ -0,0 +1,15 @@
+from wagtail.wagtailcore import hooks
+
+
+def allow_blindly(tag):
+    return tag
+
+# See: http://docs.wagtail.io/en/v1.6/reference/hooks.html#construct-whitelister-element-rules
+@hooks.register('construct_whitelister_element_rules')
+def whitelister_element_rules():
+    rules = {}
+    # Tables
+    rules.update(dict.fromkeys(['table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td'], allow_blindly))
+    # Divs, spans, code and anchors
+    rules.update(dict.fromkeys(['div', 'span', 'a', 'code', 'pre'], allow_blindly))
+    return rules
