20b3ae3d0f894c086adad6b374f2119c3c7bb67b
Henri Kotkanen <henri.kotkanen@gmail.com>
2017-01-25 18:28:41 +0200

    added logic for updating varaukset KPI in management command

--- digi/management/commands/update_indicators.py
+++ digi/management/commands/update_indicators.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
                     resp = requests.get('http://hri.fi/api/3/action/package_list')
                     indicator.value = len(resp.json()['result'])
                 except Exception as e:
-                    raise CommandError('Something went wrong while updating HRI datasets indicator, id {}. Retaining previous value {}.'.format(indicator.id, indicator.value))
+                    raise CommandError('Something went wrong while updating indicator {}. Retaining previous value {}.'.format(indicator.slug, indicator.value))
 
             #OPEN311 ISSUES
             elif indicator.slug == 'palaute':
@@ -31,7 +31,15 @@ class Command(BaseCommand):
                     # resp = requests.get('https://asiointi.hel.fi/palautews/rest/v1/requests.json?status=closed&start_date={}'.format(today))
                     indicator.value = len(resp.json())
                 except Exception as e:
-                    raise CommandError('Something went wrong while updating Open311 issues indicator, id {}. Retaining previous value {}.'.format(indicator.id, indicator.value))
+                    raise CommandError('Something went wrong while updating indicator {}. Retaining previous value {}.'.format(indicator.slug, indicator.value))
+
+            elif indicator.slug == 'varaukset':
+                try:
+                    resp = requests.get('http://api.hel.fi/respa/v1/reservation?all=true')
+                    indicator.value = resp.json()['count']
+                except Exception as e:
+                    raise CommandError('Something went wrong while updating indicator {}. Retaining previous value {}.'.format(indicator.slug, indicator.value))
+
 
             indicator.save()
             self.stdout.write(self.style.SUCCESS('Successfully updated indicator {}. Value is now {}'.format(indicator.slug, indicator.value)))
