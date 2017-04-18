dedf48ba42c2ff7b03f17ebac05d38eacb1d5c8b
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-04-18 18:38:24 +0300

    Remove HRI content related test

--- digihel/tests/test_front.py
+++ digihel/tests/test_front.py
@@ -6,4 +6,4 @@ def test_front_page(client, home_page):
     response = client.get('/')
     assert response.status_code == 200
     print(response.content)
-    assert 'Helsinki Region Infoshare' in str(response.content)
+    # assert 'Helsinki Region Infoshare' in str(response.content)
