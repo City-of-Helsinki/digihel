7fd865342cb921927065269fffbc310d98b6ef4a
Tero Tikkanen <tero.tikkanen@gmail.com>
2017-05-25 15:33:38 +0300

    Add show in sub menus option on kehmet content page. Modify side menu to respect this setting.

--- kehmet/templatetags/kehmet_tags.py
+++ kehmet/templatetags/kehmet_tags.py
@@ -7,39 +7,43 @@ register = template.Library()
 
 
 def list_children(page, target_page):
-    if page.specific == target_page:
-        is_me = True
-    else:
-        is_me = False
-    if target_page.is_descendant_of(page):
-        is_parent = True
-    else:
-        is_parent = False
 
-    children_html = ''
-    if is_parent or is_me:
-        children = page.get_children().live().public().order_by('path')
-        for child in children:
-            children_html += list_children(child, target_page)
+    if getattr(page.specific, 'show_in_submenus', True):
+        if page.specific == target_page:
+            is_me = True
+        else:
+            is_me = False
+        if target_page.is_descendant_of(page):
+            is_parent = True
+        else:
+            is_parent = False
 
-    if is_me:
-        klass = 'active'
-    elif is_parent:
-        klass = 'open'
+        children_html = ''
+        if is_parent or is_me:
+            children = page.get_children().live().public().order_by('path')
+            for child in children:
+                children_html += list_children(child, target_page)
+
+        if is_me:
+            klass = 'active'
+        elif is_parent:
+            klass = 'open'
+        else:
+            klass = ''
+        if klass:
+            klass = ' class="{}"'.format(klass)
+
+        html = '<ul><li{klass}><a href="{url}">{title}</a></li>'\
+            .format(url=page.url, title=page.title, klass=klass)
+        html += children_html
+        html += '</ul>'
+        return html
     else:
-        klass = ''
-    if klass:
-        klass = ' class="{}"'.format(klass)
-
-    html = '<ul><li{klass}><a href="{url}">{title}</a></li>'\
-        .format(url=page.url, title=page.title, klass=klass)
-    html += children_html
-    html += '</ul>'
-    return html
+        return ""
 
 
 @register.simple_tag
-def sidebar_page_nav(page):
+def kehmet_sidebar_page_nav(page):
     parent = page
     while parent is not None and not isinstance(parent.specific, KehmetFrontPage):
         parent = parent.get_parent()
