e3c4e5eb44ffa8ad6936e981914a82c2a3e3a9fb
Eetu Häivälä <eetu.haivala@anders.fi>
2018-02-15 13:47:38 +0200

    Add utility function for getting list of substrings from string

--- helsinkioppii/utils.py
+++ helsinkioppii/utils.py
@@ -15,3 +15,22 @@ def strip_dangerous_html(html):
     html = re.sub(on_pattern, '', html)
     html = re.sub(script_pattern, '', html)
     return re.sub(iframe_pattern, '', html)
+
+
+def get_substrings(string, delimiter=';', strip=True):
+    """
+    Returns a list of substrings extracted from given string that are
+    separated by given delimiter.
+
+    :param string: String from which the substrings are looked up from.
+    :param delimiter: Character (sequence) that break up multiple substrings.
+    :param strip: Flag for stripping leading and trailing whitespace from the
+                  substring.
+    :return: List of substrings
+    """
+    substrings = []
+    for substring in string.split(delimiter):
+        if strip:
+            substring = substring.strip()
+        substrings.append(substring)
+    return substrings
