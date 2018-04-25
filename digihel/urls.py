cb9091062dbcb64b90ea8209e7aba88b48a91dc9
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2018-04-25 15:36:27 +0300

    Make magic link support optional

--- digihel/urls.py
+++ digihel/urls.py
@@ -12,8 +12,6 @@ from search import views as search_views
 
 admin.autodiscover()
 
-from magicuser import urls as magic_urls
-
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
 
@@ -28,10 +26,13 @@ urlpatterns = [
 
     # client endpoints for external API data
     url(r'^event_data/', event_data),
-    url(r'^magic/', include(magic_urls)),
     url(r'', include(wagtail_urls)),
 ]
 
+# Use magic links app's URLs if available
+if 'magicuser' in settings.INSTALLED_APPS:
+    from magicuser import urls as magic_urls
+    urlpatterns = [url(r'^magic/', include(magic_urls))] + urlpatterns
 
 if settings.DEBUG:
     from django.conf.urls.static import static
